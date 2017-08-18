a99eb2af84b852eaf661100b796f5b9dc5bde300
Noel Ray Barron <Barron@Barrons-MacBook-Pro.local>
2017-08-17 22:14:50 -0700

    Minor UI fixes to the loginScreen
    In this change I implemented more constants.
    This is so that future fixes for localization and
    porting over general constants
    
    As well, I changed the implementation of the
    button so that it stays completely consistent
    accross all platforms, all we have to think about
    now is how to make it more 'native feeling'

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -14,7 +14,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  Button,
+  TouchableHighlight,
   View
 } from 'react-native';
 
@@ -23,32 +23,46 @@ const LoginScreenConstants = {
     username: "Username",
     password: "Password",
     login: "Login"
-  }
+  },
+  colors: {
+    background: 'rgb(122,204,122)',
+    loginButton: 'rgb(143,204,143)'
+  },
+  fontSize: 12
 }
 
 export class LoginScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'Useless Placeholder',
+      username: '',
       password: ''
     };
+  }
+
+  signIn = () => {
+    // TODO: Implement authentication logic
+    // Temporarily, it should be enough to fake it and navigate to the main view
   }
 
   render() {
     return (
       <View style={loginViewStyles.loginContainer}>
-        <TextInput style={[loginViewStyles.authenticationInput, loginViewStyles.usernameInput]}
-                   placeholder={LoginScreenConstants.labels.username}/>
-        <TextInput style={[loginViewStyles.authenticationInput, loginViewStyles.passwordInput]}
-                   placeholder={LoginScreenConstants.labels.password}/>
-        <Button title={LoginScreenConstants.labels.login}
-                raised
-                buttonStyle={loginViewStyles.loginButton}
-                textStyle={loginViewStyles.loginButtonText}
-                onPress= { () => {
-                  // Do something
-                }}/>
+        <TextInput
+          style={[loginViewStyles.authenticationInput, loginViewStyles.usernameInput]}
+          placeholder={LoginScreenConstants.labels.username}/>
+        <TextInput
+          style={[loginViewStyles.authenticationInput, loginViewStyles.passwordInput]}
+          placeholder={LoginScreenConstants.labels.password}/>
+        <TouchableHighlight
+          style={loginViewStyles.loginButton}
+          activeOpacity={0.7}
+          underlayColor={LoginScreenConstants.colors.loginButton}
+          onPress={this.signIn}>
+          <Text style={loginViewStyles.loginButtonText}>
+            {LoginScreenConstants.labels.login}
+          </Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -59,13 +73,13 @@ const loginViewStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgb(122,204,122)',
+    backgroundColor: LoginScreenConstants.colors.background,
   },
   authenticationInput: {
     height: 30,
     width: '75%',
     paddingLeft: 12,
-    fontSize: 12,
+    fontSize: LoginScreenConstants.fontSize,
     textAlign: 'left',
     backgroundColor: 'white',
   },
@@ -75,17 +89,21 @@ const loginViewStyles = StyleSheet.create({
   },
   passwordInput: {
     borderTopWidth: 1,
-    borderTopColor: 'rgb(122,204,122)',
+    borderTopColor: LoginScreenConstants.colors.background,
     borderBottomLeftRadius: 5,
     borderBottomRightRadius: 5
   },
   loginButton: {
-    height: 25,
-    borderColor: 'white',
-    textAlign: 'left'
+    height: 30,
+    width: '75%',
+    marginTop: 15,
+    borderRadius: 5,
+    backgroundColor: LoginScreenConstants.colors.loginButton,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   loginButtonText: {
     color: 'white',
-    fontSize: 30
+    fontSize: LoginScreenConstants.fontSize
   }
 });
